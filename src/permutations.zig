90724f3917670714438598a94090983c237663bd
Travis Staloch <twostepted@gmail.com>
2021-05-16 00:13:04 -0700

    misc
    - add to factorial tests
    - add doc comment for permuations changes

--- src/permutations.zig
+++ src/permutations.zig
@@ -72,22 +72,24 @@ fn factorialLookup(comptime T: type, n: anytype, table: anytype, limit: anytype)
 
 test "factorial" {
     inline for (.{
-        .{ i8, 5 },
-        .{ u8, 5 },
-        .{ i16, 7 },
-        .{ u16, 8 },
-        .{ i32, 12 },
-        .{ u32, 12 },
-        .{ i64, 20 },
-        .{ u64, 20 },
-        .{ isize, 20 },
-        .{ usize, 20 },
-        .{ i128, 33 },
-        .{ u128, 34 },
+        .{ i8, 5, 120 },
+        .{ u8, 5, 120 },
+        .{ i16, 7, 5040 },
+        .{ u16, 8, 40320 },
+        .{ i32, 12, 479001600 },
+        .{ u32, 12, 479001600 },
+        .{ i64, 20, 2432902008176640000 },
+        .{ u64, 20, 2432902008176640000 },
+        .{ isize, 20, 2432902008176640000 },
+        .{ usize, 20, 2432902008176640000 },
+        .{ i128, 33, 8683317618811886495518194401280000000 },
+        .{ u128, 34, 295232799039604140847618609643520000000 },
     }) |s| {
         const T = s[0];
         const max = s[1];
-        const f = try factorial(T, @as(usize, max));
+        const expected = s[2];
+        const actual = try factorial(T, @as(usize, max));
+        try std.testing.expectEqual(@as(T, expected), actual);
     }
 }
 
@@ -98,41 +100,43 @@ test "factorialBig" {
         defer f.deinit();
         try std.testing.expectEqual(fact_table128[fact_table_size_128 - 1], try f.to(u128));
     }
-    // beyond table
-    const expected_factorials = .{
-        .{ 35, 10333147966386144929666651337523200000000 },
-        .{ 36, 371993326789901217467999448150835200000000 },
-        .{ 37, 13763753091226345046315979581580902400000000 },
-        .{ 38, 523022617466601111760007224100074291200000000 },
-        .{ 39, 20397882081197443358640281739902897356800000000 },
-        .{ 40, 815915283247897734345611269596115894272000000000 },
-        .{ 41, 33452526613163807108170062053440751665152000000000 },
-        .{ 42, 1405006117752879898543142606244511569936384000000000 },
-        .{ 43, 60415263063373835637355132068513997507264512000000000 },
-        .{ 44, 2658271574788448768043625811014615890319638528000000000 },
-        .{ 45, 119622220865480194561963161495657715064383733760000000000 },
-    };
 
-    inline for (expected_factorials) |n_expected_fact| {
-        const N = n_expected_fact[0];
-        const EXPECTED = n_expected_fact[1];
-        var f = try factorialBig(N, std.testing.allocator);
-        defer f.deinit();
-        var expected = try bigint.Managed.initSet(std.testing.allocator, EXPECTED);
-        defer expected.deinit();
-        try std.testing.expect(expected.toConst().eq(f.toConst()));
+    // beyond table
+    {
+        const expected_factorials = .{
+            .{ 35, 10333147966386144929666651337523200000000 },
+            .{ 36, 371993326789901217467999448150835200000000 },
+            .{ 37, 13763753091226345046315979581580902400000000 },
+            .{ 38, 523022617466601111760007224100074291200000000 },
+            .{ 39, 20397882081197443358640281739902897356800000000 },
+            .{ 40, 815915283247897734345611269596115894272000000000 },
+            .{ 41, 33452526613163807108170062053440751665152000000000 },
+            .{ 42, 1405006117752879898543142606244511569936384000000000 },
+            .{ 43, 60415263063373835637355132068513997507264512000000000 },
+            .{ 44, 2658271574788448768043625811014615890319638528000000000 },
+            .{ 45, 119622220865480194561963161495657715064383733760000000000 },
+        };
+
+        inline for (expected_factorials) |n_ex| {
+            const N = n_ex[0];
+            const EXPECTED = n_ex[1];
+            var f = try factorialBig(N, std.testing.allocator);
+            defer f.deinit();
+            var expected = try bigint.Managed.initSet(std.testing.allocator, EXPECTED);
+            defer expected.deinit();
+            try std.testing.expect(expected.toConst().eq(f.toConst()));
+        }
     }
 }
 
+/// for sets of length 35 and less
 pub fn nthperm(a: anytype, n: u6) !void {
     if (a.len == 0) return;
 
     var f = try factorial(u128, a.len);
-
     if (n > f) return error.ArgumentBounds;
 
     var i: usize = 0;
-
     var nmut = @as(u128, n);
     while (i < a.len) : (i += 1) {
         f = f / (a.len - i);
@@ -193,6 +197,7 @@ test "nthperm" {
     );
 }
 
+/// for sets of any size
 pub fn nthpermBig(a: anytype, n: usize, allocator: *std.mem.Allocator) !void {
     if (a.len == 0) return;
 
@@ -238,10 +243,11 @@ pub fn nthpermBig(a: anytype, n: usize, allocator: *std.mem.Allocator) !void {
     }
 }
 
+const initial_state_big = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij";
+
 test "nthpermBig" {
-    const initial_state = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij";
-    try std.testing.expectEqual(fact_table_size_128 + 1, initial_state.len);
-    var buf = initial_state.*;
+    try std.testing.expectEqual(fact_table_size_128 + 1, initial_state_big.len);
+    var buf = initial_state_big.*;
     try nthpermBig(&buf, std.math.maxInt(usize) - 1, std.testing.allocator);
     try std.testing.expectEqualStrings("ABCDEFGHIJKLMNOWbdTSjUYVaPhZfQRXgeci", &buf);
 
@@ -252,10 +258,13 @@ test "nthpermBig" {
     );
 }
 
+/// for sets of length 35 and less
 pub fn Permutations(comptime T: type) type {
     return struct {
         i: usize,
         initial_state: []const u8,
+        /// must be at least as long as initial state.
+        /// initial_state will be copied to this buffer each time next() is called.
         buf: []T,
         const Self = @This();
 
@@ -272,12 +281,69 @@ pub fn Permutations(comptime T: type) type {
     };
 }
 
-test "iterator" {
+test "Permutations iterator" {
     var buf: [4]u8 = undefined;
     var it = Permutations(u8).init("ABCA", &buf);
     var i: u8 = 0;
     while (it.next()) |actual| : (i += 1) {
         const expected = expecteds[i];
         try std.testing.expectEqualStrings(expected, actual);
+    }
+}
+
+/// for sets of any size, 
+pub fn PermutationsBig(comptime T: type) type {
+    return struct {
+        i: usize,
+        initial_state: []const u8,
+        buf: []T,
+        allocator: *std.mem.Allocator,
+        const Self = @This();
+
+        pub fn init(initial_state: []const T, buf: []T, allocator: *std.mem.Allocator) Self {
+            return .{ .i = 0, .initial_state = initial_state, .buf = buf, .allocator = allocator };
+        }
+
+        pub fn next(self: *Self) !?[]const T {
+            std.mem.copy(u8, self.buf, self.initial_state);
+            try nthpermBig(self.buf, @intCast(u6, self.i), self.allocator);
+            self.i += 1;
+            return self.buf;
+        }
+    };
+}
+
+const expecteds_big: []const []const u8 = &.{
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghij",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghji",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgihj",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgijh",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgjhi",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefgjih",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhgij",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhgji",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhigj",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijg",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhjgi",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhjig",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefighj",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefigjh",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefihgj",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefihjg",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefijgh",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefijhg",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefjghi",
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefjgih",
+};
+
+test "PermutationsBig iterator" {
+    var buf = initial_state_big.*;
+
+    var it = PermutationsBig(u8).init(initial_state_big, &buf, std.testing.allocator);
+    var i: u8 = 0;
+    while (try it.next()) |actual| : (i += 1) {
+        const expected = expecteds_big[i];
+        try std.testing.expectEqualStrings(expected, actual);
+        if (i >= expecteds_big.len - 1) break;
     }
 }
